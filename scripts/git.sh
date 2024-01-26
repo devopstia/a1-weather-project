06928e56cd49f7705149b9a5dfa33df03768d1ca
Tia Leo <info@devopseasylearning>
2024-01-25 19:42:41 -0600

    DevOps Easy Learning

---
+++ scripts/git.sh
@@ -0,0 +1,4 @@
+read -r -p 'Commit message: ' desc  # prompt user for commit message
+git add .                           # track all files
+git commit -m "$desc"               # commit with message
+git push -u origin master           # push to origin
